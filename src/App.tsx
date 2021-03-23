3b15f57846b11f1d38c7f217f62cecae74209bdd
ualetodev <ualetodev@pm.me>
2021-03-23 03:31:16 -0300

    update content

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>ualeto started.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
