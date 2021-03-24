532812b460888e79b7dc38e4404a1d2dcbffff15
ualetodev <ualetodev@pm.me>
2021-03-24 20:45:49 -0300

    add not found content

--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -1,8 +1,29 @@
+import { Button, Heading, LogoIcon, Text } from '@pancakeswap-libs/uikit';
 import Page from 'components/layout/Page';
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledNotFound = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 64px);
+  justify-content: center;
+`;
 
 const NotFound: React.FC = () => {
-  return <Page>NotFound</Page>;
+  return (
+    <Page>
+      <StyledNotFound>
+        <LogoIcon width="64px" mb="8px" />
+        <Heading size="xxl">404</Heading>
+        <Text mb="16px">Oops, page not found.</Text>
+        <Button as="a" href="/" scale="sm">
+          Back Home
+        </Button>
+      </StyledNotFound>
+    </Page>
+  );
 };
 
 export default NotFound;
