2a0ed0c5ee2676918b2000bc5992bd582488df8f
ualetodev <ualetodev@pm.me>
2021-03-23 22:37:49 -0300

    update prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 const App: React.FC = () => {
   return (
@@ -8,17 +8,12 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>ualeto started</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
